52c953bfc592f1685d0ba16a676ce631a8109424
ktoczyl <ktoczyl@gmail.com>
2016-01-05 12:24:20 -0500

    Removed old file writer, added listening for new data.

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -5,45 +5,53 @@
 
 package com.interaxon.test.libmuse;
 
-import java.io.File;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View.OnClickListener;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.interaxon.libmuse.Accelerometer;
-import com.interaxon.libmuse.AnnotationData;
 import com.interaxon.libmuse.ConnectionState;
 import com.interaxon.libmuse.Eeg;
 import com.interaxon.libmuse.LibMuseVersion;
-import com.interaxon.libmuse.MessageType;
 import com.interaxon.libmuse.Muse;
 import com.interaxon.libmuse.MuseArtifactPacket;
-import com.interaxon.libmuse.MuseConfiguration;
 import com.interaxon.libmuse.MuseConnectionListener;
 import com.interaxon.libmuse.MuseConnectionPacket;
 import com.interaxon.libmuse.MuseDataListener;
 import com.interaxon.libmuse.MuseDataPacket;
 import com.interaxon.libmuse.MuseDataPacketType;
-import com.interaxon.libmuse.MuseFileFactory;
-import com.interaxon.libmuse.MuseFileReader;
-import com.interaxon.libmuse.MuseFileWriter;
 import com.interaxon.libmuse.MuseManager;
 import com.interaxon.libmuse.MusePreset;
 import com.interaxon.libmuse.MuseVersion;
 
+/*kt: new stuff kt
+	ALPHA_ABSOLUTE);
+	ALPHA_SCORE);
+        BETA_ABSOLUTE);
+	BETA_RELATIVE);
+	BETA_SCORE);
+        DELTA_ABSOLUTE);
+	DELTA_RELATIVE);
+	DELTA_SCORE);
+        GAMMA_ABSOLUTE);
+        GAMMA_RELATIVE);
+        GAMMA_SCORE);
+        THETA_ABSOLUTE);
+        THETA_RELATIVE);
+        THETA_SCORE);
+*/
 
 /**
  * In this simple example MainActivity implements 2 MuseHeadband listeners
@@ -81,6 +89,18 @@ public class MainActivity extends Activity implements OnClickListener {
             this.activityRef = activityRef;
         }
 
+	// file stuff
+	FileWriter fstream = new FileWriter("MuseSleep_dateout.txt", true); //true tells to append data.
+	outFile = new BufferedWriter(fstream);
+	outFile.write("\nSleepMuse test write to file ");
+	finally
+	    {
+		if(outFile != null) {
+		    outFile.close();
+		}
+	    }
+	//end of file stuff
+
         @Override
         public void receiveMuseConnectionPacket(MuseConnectionPacket p) {
             final ConnectionState current = p.getCurrentConnectionState();
@@ -88,7 +108,7 @@ public class MainActivity extends Activity implements OnClickListener {
                          " -> " + current;
             final String full = "Muse " + p.getSource().getMacAddress() +
                                 " " + status;
-            Log.i("Muse Headband kt", full);
+            Log.i("Muse Headband", full);
             Activity activity = activityRef.get();
             // UI thread is used here only because we need to update
             // TextView values. You don't have to use another thread, unless
@@ -114,7 +134,7 @@ public class MainActivity extends Activity implements OnClickListener {
                             museVersionText.setText(R.string.undefined);
                         }
                     }
-                });
+                }
             }
         }
     }
@@ -131,7 +151,6 @@ public class MainActivity extends Activity implements OnClickListener {
     class DataListener extends MuseDataListener {
 
         final WeakReference<Activity> activityRef;
-        private MuseFileWriter fileWriter;
 
         DataListener(final WeakReference<Activity> activityRef) {
             this.activityRef = activityRef;
@@ -140,24 +159,32 @@ public class MainActivity extends Activity implements OnClickListener {
         @Override
         public void receiveMuseDataPacket(MuseDataPacket p) {
             switch (p.getPacketType()) {
-                case EEG:
-                    updateEeg(p.getValues());
-                    break;
-                case ACCELEROMETER:
-                    updateAccelerometer(p.getValues());
-                    break;
-                case ALPHA_RELATIVE:
-                    updateAlphaRelative(p.getValues());
-                    break;
-                case BATTERY:
-                    fileWriter.addDataPacket(1, p);
-                    // It's library client responsibility to flush the buffer,
-                    // otherwise you may get memory overflow. 
-                    if (fileWriter.getBufferedMessagesSize() > 8096)
-                        fileWriter.flush();
-                    break;
-                default:
-                    break;
+	    case EEG:
+		updateEeg(p.getValues());
+		break;
+	    case ACCELEROMETER:
+		updateAccelerometer(p.getValues());
+		break;
+	    case ALPHA_RELATIVE:
+		updateAlphaRelative(p.getValues());
+		break;
+	    case ALPHA_ABSOLUTE:
+	    case ALPHA_SCORE:
+	    case BETA_ABSOLUTE:
+	    case BETA_RELATIVE:
+	    case BETA_SCORE:
+	    case DELTA_ABSOLUTE:
+	    case DELTA_RELATIVE:
+	    case DELTA_SCORE:
+	    case GAMMA_ABSOLUTE:
+	    case GAMMA_RELATIVE:
+	    case GAMMA_SCORE:
+	    case THETA_ABSOLUTE:
+	    case THETA_RELATIVE:
+	    case THETA_SCORE:
+		hadleWaivePacket(p);
+	    default:
+		break;
             }
         }
 
@@ -167,6 +194,44 @@ public class MainActivity extends Activity implements OnClickListener {
                 Log.i("Artifacts", "blink");
             }
         }
+	private  hadleWaivePacket(MuseDataPacket p);
+	{
+	    switch (p.getPacketType()) {
+	    case ALPHA_ABSOLUTE:
+		break;
+	    case ALPHA_SCORE:
+		break;
+	    case BETA_ABSOLUTE:
+		break;
+	    case BETA_RELATIVE:
+		break;
+	    case BETA_SCORE:
+		break;
+	    case DELTA_ABSOLUTE:
+		break;
+	    case DELTA_RELATIVE:
+		break;
+	    case DELTA_SCORE:
+		break;
+	    case GAMMA_ABSOLUTE:
+		break;
+	    case GAMMA_RELATIVE:
+		break;
+	    case GAMMA_SCORE:
+		break;
+	    case THETA_ABSOLUTE:
+		break;
+	    case THETA_RELATIVE:
+		break;
+	    case THETA_SCORE:
+		break;
+	    }
+	    // write/append data to file
+	    // file name : <date>_museSleep.txt 02_25_2015
+	    
+
+
+	}
 
         private void updateAccelerometer(final ArrayList<Double> data) {
             Activity activity = activityRef.get();
@@ -233,23 +298,17 @@ public class MainActivity extends Activity implements OnClickListener {
                 });
             }
         }
-
-        public void setFileWriter(MuseFileWriter fileWriter) {
-            this.fileWriter  = fileWriter;
-        }
     }
 
     private Muse muse = null;
     private ConnectionListener connectionListener = null;
     private DataListener dataListener = null;
     private boolean dataTransmission = true;
-    private MuseFileWriter fileWriter = null;
 
     public MainActivity() {
         // Create listeners and pass reference to activity to them
         WeakReference<Activity> weakActivity =
                                 new WeakReference<Activity>(this);
-
         connectionListener = new ConnectionListener(weakActivity);
         dataListener = new DataListener(weakActivity);
     }
@@ -266,22 +325,7 @@ public class MainActivity extends Activity implements OnClickListener {
         disconnectButton.setOnClickListener(this);
         Button pauseButton = (Button) findViewById(R.id.pause);
         pauseButton.setOnClickListener(this);
-        // // Uncommet to test Muse File Reader
-        //
-        // // file can be big, read it in a separate thread
-        // Thread thread = new Thread(new Runnable() {
-        //     public void run() {
-        //         playMuseFile("testfile.muse");
-        //     }
-        // });
-        // thread.start();
-
-        File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-        fileWriter = MuseFileFactory.getMuseFileWriter(
-                     new File(dir, "new_muse_file.muse"));
         Log.i("Muse Headband", "libmuse version=" + LibMuseVersion.SDK_VERSION);
-        fileWriter.addAnnotationString(1, "MainActivity onCreate");
-        dataListener.setFileWriter(fileWriter);
     }
 
     @Override
@@ -311,13 +355,10 @@ public class MainActivity extends Activity implements OnClickListener {
                 ConnectionState state = muse.getConnectionState();
                 if (state == ConnectionState.CONNECTED ||
                     state == ConnectionState.CONNECTING) {
-                    Log.w("Muse Headband",
-                    "doesn't make sense to connect second time to the same muse");
+                    Log.w("Muse Headband", "doesn't make sense to connect second time to the same muse");
                     return;
                 }
-                configureLibrary();
-                fileWriter.open();
-                fileWriter.addAnnotationString(1, "Connect clicked");
+                configure_library();
                 /**
                  * In most cases libmuse native library takes care about
                  * exceptions and recovery mechanism, but native code still
@@ -343,9 +384,6 @@ public class MainActivity extends Activity implements OnClickListener {
                  * muse.disconnect(false);
                  */
                 muse.disconnect(true);
-                fileWriter.addAnnotationString(1, "Disconnect clicked");
-                fileWriter.flush();
-                fileWriter.close();
             }
         }
         else if (v.getId() == R.id.pause) {
@@ -356,61 +394,57 @@ public class MainActivity extends Activity implements OnClickListener {
         }
     }
 
-    /*
-     * Simple example of getting data from the "*.muse" file
-     */
-    private void playMuseFile(String name) {
-        File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-        File file = new File(dir, name);
-        final String tag = "Muse File Reader";
-        if (!file.exists()) {
-            Log.w(tag, "file doesn't exist");
-            return;
-        }
-        MuseFileReader fileReader = MuseFileFactory.getMuseFileReader(file);
-        while (fileReader.gotoNextMessage()) {
-            MessageType type = fileReader.getMessageType();
-            int id = fileReader.getMessageId();
-            long timestamp = fileReader.getMessageTimestamp();
-            Log.i(tag, "type: " + type.toString() +
-                  " id: " + Integer.toString(id) +
-                  " timestamp: " + String.valueOf(timestamp));
-            switch(type) {
-                case EEG: case BATTERY: case ACCELEROMETER: case QUANTIZATION:
-                    MuseDataPacket packet = fileReader.getDataPacket();
-                    Log.i(tag, "data packet: " + packet.getPacketType().toString());
-                    break;
-                case VERSION:
-                    MuseVersion version = fileReader.getVersion();
-                    Log.i(tag, "version" + version.getFirmwareType());
-                    break;
-                case CONFIGURATION:
-                    MuseConfiguration config = fileReader.getConfiguration();
-                    Log.i(tag, "config" + config.getBluetoothMac());
-                    break;
-                case ANNOTATION:
-                    AnnotationData annotation = fileReader.getAnnotation();
-                    Log.i(tag, "annotation" + annotation.getData());
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-
-    private void configureLibrary() {
+    private void configure_library() {
         muse.registerConnectionListener(connectionListener);
         muse.registerDataListener(dataListener,
                                   MuseDataPacketType.ACCELEROMETER);
         muse.registerDataListener(dataListener,
                                   MuseDataPacketType.EEG);
+	/* kt: moved to below
         muse.registerDataListener(dataListener,
                                   MuseDataPacketType.ALPHA_RELATIVE);
+	*/
         muse.registerDataListener(dataListener,
                                   MuseDataPacketType.ARTIFACTS);
-        muse.registerDataListener(dataListener,
-                                  MuseDataPacketType.BATTERY);
+
+	//kt: new stuff kt
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.ALPHA_ABSOLUTE);
+	muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.ALPHA_RELATIVE);
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.ALPHA_SCORE);
+
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.BETA_ABSOLUTE);
+	muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.BETA_RELATIVE);
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.BETA_SCORE);
+
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.DELTA_ABSOLUTE);
+	muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.DELTA_RELATIVE);
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.DELTA_SCORE);
+
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.GAMMA_ABSOLUTE);
+	muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.GAMMA_RELATIVE);
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.GAMMA_SCORE);
+
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.THETA_ABSOLUTE);
+	muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.THETA_RELATIVE);
+	muse.registerDataListener(dataListener,
+				  MuseDataPacketType.THETA_SCORE);
+
+
+
         muse.setPreset(MusePreset.PRESET_14);
         muse.enableDataTransmission(dataTransmission);
     }
